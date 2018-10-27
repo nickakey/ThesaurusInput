02cf1900f52787a7fbbe702a1c80b71c957f4b3f
Nick Akey <nicktheflanders@gmail.com>
2018-10-27 10:00:59 -0500

    boilerplate done

---
+++ CLIENT/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import axios from "axios";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+      return (
+        <div>
+          WORD ROLLER
+        </div>
+      )
+  } 
+}
+export default App;
